d30cb4cc14fca6d2bbcd52c2ff76cc1f6fdb72fc
BunningsSnags <36836011+BunningsSnags@users.noreply.github.com>
2019-07-26 19:18:05 +1000

    Timer Library Added

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,54 @@
+// ============ Includes ============
 #include <LRFs.h>
 #include <Motors.h>
+#include <Common.h>
+#include <Timer.h>
 
+// ============ Setups ============
 LRFs lrfs;
 Motors motors;
 
-void setup() {
-    lrfs.init();
-    motors.init();
+    // ------------ Timers ------------
+Timer RGBTimer(800000);
+Timer LEDTimer(800000);
+bool ledOn = false;
+
+    // ------------ LED Flash ------------
+void flash(){
+    if(LEDTimer.timeHasPassed()){
+        digitalWrite(ledPin, ledOn);
+        ledOn = !ledOn;
+    }
+
 }
 
-void loop() {
-    lrfs.update();
+    // ------------ LRF Print ------------
+void printer(){
     for(int i = 0; i < LRF_NUM; i++) {
         Serial.print(lrfs.value[i]);
         Serial.print("\t");
     }
-    Serial.println();
-    motors.update(100, 100);
+    Serial.println("Front-Left, Front-Right, Left-Left, Left-Right, Back-Left, Back-Right, Right-Left, Right-Right");
+}
+    // ------------ Normal Setup ------------
+void setup() {
+    lrfs.init();
+    motors.init();
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, LOW);
+}
+
+// ============ Main ============
+void loop() {
+    // ------------ Debuging ------------
+    while(true){
+        printer();
+        flash();
+    }
+    motors.update(-50, 50);
+
+
+
+
 }
