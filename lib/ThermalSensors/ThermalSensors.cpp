2492bfb1e7261a458ab65c27bd285f7da9c8f748
BunningsSnags <36836011+BunningsSnags@users.noreply.github.com>
2019-07-26 22:42:51 +1000

    Thermal Sensor update
    Removed unwanted comments

--- lib/ThermalSensors/ThermalSensors.cpp
+++ lib/ThermalSensors/ThermalSensors.cpp
@@ -1,20 +1,4 @@
-/******************************************************************************
-SparkFunMLX90614.cpp
-Source file for the SparkFun IR Thermometer Library
-
-Jim Lindblom @ SparkFun Electronics
-October 23, 2015
-https://github.com/sparkfun/SparkFun_MLX90614_Arduino_Library
-
-This file defines the SMBus hardware interface(s) for the MLX90614 IR thermometer
-and abstracts temperature measurments and other features of the MLX90614
-
-Development environment specifics:
-Arduino 1.6.5
-SparkFun IR Thermometer Evaluation Board - MLX90614
-******************************************************************************/
-
-#include <SparkFunMLX90614.h>
+#include <ThermalSensors.h>
 
 IRTherm::IRTherm()
 {
--- lib/ThermalSensors/ThermalSensors.h
+++ lib/ThermalSensors/ThermalSensors.h
@@ -1,21 +1,5 @@
-/****************************************************************************** 
-SparkFunMLX90614.h
-Header file for the SparkFun IR Thermometer Library
-
-Jim Lindblom @ SparkFun Electronics
-October 23, 2015
-https://github.com/sparkfun/SparkFun_MLX90614_Arduino_Library
-
-This file defines the SMBus hardware interface(s) for the MLX90614 IR thermometer
-and abstracts temperature measurments and other features of the MLX90614
-
-Development environment specifics:
-Arduino 1.6.5
-SparkFun IR Thermometer Evaluation Board - MLX90614
-******************************************************************************/
-
-#ifndef SPARKFUNMLX90164_H
-#define SPARKFUNMLX90164_H
+#ifndef THERMALSENSORS_H
+#define THERMALSENSORS_H
 
 #include <Arduino.h>
 #include <Wire.h>
