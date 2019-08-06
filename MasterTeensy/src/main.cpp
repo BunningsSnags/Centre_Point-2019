fb28cf5b43e252b4c5100b8cbc49a9a5385e8508
Pat Telfer <36836011+BunningsSnags@users.noreply.github.com>
2019-08-06 15:28:27 +1000

    Motor Direction Test

--- MasterTeensy/src/main.cpp
+++ MasterTeensy/src/main.cpp
@@ -7,12 +7,5 @@ void setup() {
 }
 
 void loop() {
-    motors.update(100, 100);
-    delay(4000);
-    motors.update(-100, 100);
-    delay(2000);
-    motors.update(-100, -100);
-    delay(4000);
-    motors.update(100, -100);
-    delay(2000)
+    
 }
