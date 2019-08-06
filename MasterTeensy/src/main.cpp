8a88f963e294c8427ed4db34479107e5bf48f9c3
Pat Telfer <36836011+BunningsSnags@users.noreply.github.com>
2019-08-07 09:03:25 +1000

    Tile Move Function added

--- MasterTeensy/src/main.cpp
+++ MasterTeensy/src/main.cpp
@@ -1,11 +1,47 @@
 #include <MotorController.h>
+#include <LRFs.h>
 
 MotorController motors;
+LRFs lrfs;
+
+void lrfPrint() {
+    for(int i = 0; i < LRF_NUM; i++) {
+        Serial.print(lrfs.value[i]);
+        Serial.print("\t");
+    }
+    Serial.println("Front-Left, ");
+}
 
 void setup() {
     motors.init();
+    lrfs.init();
+}
+
+void tileMove(char direction) {
+    if(direction == "forward") {
+        motors.update(100, 100);
+        delay(TILE_DIST_SEC)
+    } else if(direction == "back") {
+        motors.update(-100, -100);
+        delay(TILE_DIST_SEC)
+    } else if(direction == "left") {
+        motors.update(-100, 100);
+        //Function for imu degrees(TILE_TURN_DEG)
+    } else if(direction == "right") {
+        motors.update(-100, 100);
+        //Function for imu degrees(TILE_TURN_DEG)
+    } else {
+        Serial.printnl('Unknown direction, please use:')
+        Serial.printnl('"forward", "back", "left" or "right"')
+    }
 }
 
 void loop() {
-    
+    lrfs.update();
+    // lrfPrint();
+    while(lrfs.value[0] > 200) {
+        lrfs.update();
+        motors.update(100, 100);
+    }
+    motors.update(-150, 150);
 }
--- lib/Common/Common.h
+++ lib/Common/Common.h
@@ -8,4 +8,9 @@
 #define LRF_NUM 8
 #define LRF_PACKET_SIZE 8
 
+// Tile Spacing
+#define TILE_DIST_SEC 2000
+#define TILE_TURN_SEC
+#define TILE_TURN_DEG
+
 #endif
