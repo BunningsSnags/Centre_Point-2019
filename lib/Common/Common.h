f0c81ec49464b17e18a38160df81361fb05b3577
Aparaj Bogahawatta <24989@bbc.qld.edu.au>
2019-08-09 16:06:48 +1000

    Writing teensy coms code

--- lib/Common/Common.h
+++ lib/Common/Common.h
@@ -21,27 +21,6 @@
 #define LRF_PACKET_SIZE 8
 
 
-//MPU
-#define MPU_CALIBRATION_COUNT 20
-#define MPU_CALIBRATION_TIME 50
-#define MPU9250_ADDRESS 0x68
-#define MAG_ADDRESS 0x0C
-#define GYRO_FULL_SCALE_250_DPS 0x00
-#define GYRO_FULL_SCALE_500_DPS 0x08
-#define GYRO_FULL_SCALE_1000_DPS 0x10
-#define GYRO_FULL_SCALE_2000_DPS 0x18
-#define ACC_FULL_SCALE_2_G 0x00
-#define ACC_FULL_SCALE_4_G 0x08
-#define ACC_FULL_SCALE_8_G 0x10
-#define ACC_FULL_SCALE_16_G 0x18
-
-
-// Thermal Sensors
-#define THERMAL_NUM 4
-#define THERMAL_CALIBRATION_COUNT 5
-#define THERMAL_CALIBRATION_TIME 50
-
-
 // Tile Spacing
 #define TILE_SIZE 300
 #define TILE_DIST_TIME 1550
@@ -64,13 +43,5 @@ struct Coordinate {
     int8_t y;
     int8_t z;
 };
-
-struct Vector3D {
-	float x;
-	float y;
-	float z;
-};
-
-float floatMod(float x, float m);
 
 #endif
