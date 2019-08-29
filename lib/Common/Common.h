a9e9c18632231c2bb633680c0e8f09487d71c000
Pat Telfer <36836011+BunningsSnags@users.noreply.github.com>
2019-08-29 17:39:18 +1000

    MPU Code creation

--- lib/Common/Common.h
+++ lib/Common/Common.h
@@ -37,18 +37,40 @@
 
 
 // RGB Leds
-#define NUM_RGB_LEDS 16
-#define DATA_PIN 48
+#define NUM_RGB_LEDS 60
+#define DATA_PIN 23
 
 
 // Stepper
 #define STEPPER_BAUD_RATE 115200
 
 
+//MPU
+#define MPU_CALIBRATION_COUNT 20
+#define MPU_CALIBRATION_TIME 50
+#define MPU9250_ADDRESS 0x68
+#define MAG_ADDRESS 0x0C
+#define GYRO_FULL_SCALE_250_DPS 0x00
+#define GYRO_FULL_SCALE_500_DPS 0x08
+#define GYRO_FULL_SCALE_1000_DPS 0x10
+#define GYRO_FULL_SCALE_2000_DPS 0x18
+#define ACC_FULL_SCALE_2_G 0x00
+#define ACC_FULL_SCALE_4_G 0x08
+#define ACC_FULL_SCALE_8_G 0x10
+#define ACC_FULL_SCALE_16_G 0x18
+
 struct Coordinate {
     int8_t x;
     int8_t y;
     int8_t z;
 };
+
+struct Vector3D {
+	float x;
+	float y;
+	float z;
+};
+
+float floatMod(float x, float m);
 
 #endif
