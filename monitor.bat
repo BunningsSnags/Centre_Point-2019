aa459bb23f8611bce091726e4308b1f61705f237
Pat Telfer <36836011+BunningsSnags@users.noreply.github.com>
2019-09-30 15:45:28 +1000

    Create monitor.bat

---
+++ monitor.bat
@@ -0,0 +1,14 @@
+@echo off
+set input=%1
+if %input% == master (
+  echo Monitoring Master
+  cd MasterTeensy
+  pio device monitor
+  cd ..
+)
+if %input% == slave (
+  echo Monitoring Slave
+  cd SlaveTeensy
+  pio device monitor
+  cd..
+)
